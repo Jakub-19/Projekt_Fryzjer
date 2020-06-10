6246e9d3ce28cdf491e055a83fa8e1d3ef156979
frivoloussav <wow589@o2.pl>
2020-06-10 02:38:17 +0200

    Dodano słowniki

--- FryzjerManager/ViewModel/MainWindowViewModel.cs
+++ FryzjerManager/ViewModel/MainWindowViewModel.cs
@@ -10,25 +10,45 @@ namespace FryzjerManager.ViewModel
 {
     using RelayCommand = ViewModelBase.RelayCommand;
     using V = Views;
+    using R = Properties.Resources;
+
     public class MainWindowViewModel : ViewModelBase.ViewModelBase
     {
         private ICommand _gotoViewMenuWindowCommand;
+        private ICommand _gotoViewCustomersCommand;
         private ICommand _gotoViewCustomerAddCommand;
         private ICommand _gotoViewServiceDoneCommand;
         private object _currentView;
         private object _viewMenuWindow;
+        private object _viewCustomers;
         private object _viewCustomerAdd;
         private object _viewServiceDone;
 
         public MainWindowViewModel()
         {
             _viewMenuWindow = new V.ViewMenuWindow();
+            _viewCustomers = new V.ViewCustomers();
             _viewCustomerAdd = new V.ViewCustomerAdd();
             _viewServiceDone = new V.ViewServiceDone();
 
             CurrentView = _viewMenuWindow;
         }
-
+        #region ResourcesTextNames
+        public string ViewMenuWindowCustomersButtonText{
+            get{ return R.ViewMenuWindowCustomersButtonText; }}
+        public string ViewMenuWindowServicesButtonText{
+            get{ return R.ViewMenuWindowServicesButtonText; }}
+        public string ViewMenuWindowStockButtonText{
+            get { return R.ViewMenuWindowStockButtonText; }}
+        public string ViewMenuWindowAuthorsButtonText{
+            get { return R.ViewMenuWindowAuthorsButtonText; }}
+        public string ApplicationName{
+            get { return R.ApplicationName; }}
+        public string ViewCustomersAddCustomerButtonText{
+            get { return R.ViewCustomersAddCustomerButtonText; }}
+        public string ViewCustomersServicesHistoryButtonText{
+            get { return R.ViewCustomersServicesHistoryButtonText; }}
+        #endregion
         public ICommand GotoViewMenuWindowCommand
         {
             get
@@ -49,6 +69,17 @@ namespace FryzjerManager.ViewModel
                    x =>
                    {
                        GotoViewCustomerAdd();
+                   }));
+            }
+        }
+        public ICommand GotoViewCustomersCommand
+        {
+            get
+            {
+                return _gotoViewCustomersCommand ?? (_gotoViewCustomersCommand = new RelayCommand(
+                   x =>
+                   {
+                       GotoViewCustomers();
                    }));
             }
         }
@@ -78,6 +109,10 @@ namespace FryzjerManager.ViewModel
         private void GotoViewMenuWindow()
         {
             CurrentView = _viewMenuWindow;
+        }
+        private void GotoViewCustomers()
+        {
+            CurrentView = _viewCustomers;
         }
         private void GotoViewCustomerAdd()
         {
