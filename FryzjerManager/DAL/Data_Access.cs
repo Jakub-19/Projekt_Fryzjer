0b3dded9c2f8187b4c9b57e6de2b829abbb228c0
AllysBlaze <44466481+AllysBlaze@users.noreply.github.com>
2020-06-10 17:56:44 +0200

    Funkcje dla produktów

--- FryzjerManager/DAL/Data_Access.cs
+++ FryzjerManager/DAL/Data_Access.cs
@@ -99,5 +99,188 @@ namespace FryzjerManager.DAL
 
         }
         #endregion
+
+        #region Product
+        public List<SingleUseProduct> ShowAllProducts()
+        {
+            List<SingleUseProduct> products = new List<SingleUseProduct>();
+            try
+            {
+                con.Open();
+            }
+            catch { }
+            string command = "SELECT * FROM disposable_products ";
+            MySqlCommand cmd = new MySqlCommand(command, con);
+            MySqlDataReader rdr = cmd.ExecuteReader();
+            while (rdr.Read())
+            {
+                SingleUseProduct product = new SingleUseProduct(rdr.GetInt32(0), rdr.GetString(1), rdr.GetInt32(2), rdr.GetInt32(3));
+                products.Add(product);
+            }
+            rdr.Close();
+            command = "SELECT * FROM products ";
+            cmd = new MySqlCommand(command, con);
+            rdr = cmd.ExecuteReader();
+            while (rdr.Read())
+            {
+                Product product = new Product(rdr.GetInt32(0), rdr.GetString(1), rdr.GetInt32(2), rdr.GetInt32(3), rdr.GetInt32(4), rdr.GetInt32(5));
+                products.Add(product);
+            }
+            con.Close();
+
+            return products;
+        }
+
+        public void UpdateProductCount(SingleUseProduct product, int newCount)
+        {
+            try
+            {
+                con.Open();
+            }
+            catch { }
+            string command = "UPDATE products set quantity_item=" + newCount.ToString() + " Where id_p=" + product.ID.ToString();
+            MySqlCommand cmd = new MySqlCommand(command, con);
+            MySqlDataReader rdr = cmd.ExecuteReader();
+            rdr.Close();
+            command = "UPDATE disposable_products set quantity=" + newCount.ToString() + " Where id_p=" + product.ID.ToString();
+            cmd = new MySqlCommand(command, con);
+            rdr = cmd.ExecuteReader();
+            con.Close();
+        }
+        public void UpdateProductMl(Product product, int newml)
+        {
+            try
+            {
+                con.Open();
+            }
+            catch { }
+            string command = "UPDATE products set ml=" + newml.ToString() + " Where id_p=" + product.ID.ToString();
+            MySqlCommand cmd = new MySqlCommand(command, con);
+            MySqlDataReader rdr = cmd.ExecuteReader();
+            con.Close();
+        }
+        public List<SingleUseProduct> SearchProductByName(string name)
+        {
+            List<SingleUseProduct> products = new List<SingleUseProduct>();
+            try
+            {
+                con.Open();
+            }
+            catch { }
+            string command = "SELECT * FROM disposable_products where name like \"" + name + "%\"";
+            MySqlCommand cmd = new MySqlCommand(command, con);
+            MySqlDataReader rdr = cmd.ExecuteReader();
+            while (rdr.Read())
+            {
+                SingleUseProduct product = new SingleUseProduct(rdr.GetInt32(0), rdr.GetString(1), rdr.GetInt32(2), rdr.GetInt32(3));
+                products.Add(product);
+            }
+            rdr.Close();
+            command = "Select *from products where name like \"" + name + "%\"";
+            cmd = new MySqlCommand(command, con);
+            rdr = cmd.ExecuteReader();
+            while (rdr.Read())
+            {
+                Product product = new Product(rdr.GetInt32(0), rdr.GetString(1), rdr.GetInt32(2), rdr.GetInt32(3), rdr.GetInt32(4), rdr.GetInt32(5));
+                products.Add(product);
+            }
+
+            con.Close();
+            return products;
+        }
+        public List<SingleUseProduct> ShowAvailableProducts()
+        {
+            List<SingleUseProduct> products = new List<SingleUseProduct>();
+            try
+            {
+                con.Open();
+            }
+            catch { }
+            string command = "SELECT * FROM disposable_products where quantity <> 0";
+            MySqlCommand cmd = new MySqlCommand(command, con);
+            MySqlDataReader rdr = cmd.ExecuteReader();
+            while (rdr.Read())
+            {
+                SingleUseProduct product = new SingleUseProduct(rdr.GetInt32(0), rdr.GetString(1), rdr.GetInt32(2), rdr.GetInt32(3));
+                products.Add(product);
+            }
+            rdr.Close();
+            command = "SELECT * FROM products where quantity_item <> 0 and ml <> 0 ";
+            cmd = new MySqlCommand(command, con);
+            rdr = cmd.ExecuteReader();
+            while (rdr.Read())
+            {
+                Product product = new Product(rdr.GetInt32(0), rdr.GetString(1), rdr.GetInt32(2), rdr.GetInt32(3), rdr.GetInt32(4), rdr.GetInt32(5));
+                products.Add(product);
+            }
+            con.Close();
+
+            return products;
+        }
+        public void AddSingleUseProduct(SingleUseProduct product)
+        {
+            string name = product.Name;
+            int count = product.Count;
+            int price = product.Price;
+            try
+            {
+                con.Open();
+            }
+            catch { }
+            string command = "INSERT INTO disposable_products(name, quantity, price) VALUES('" + name + "','" + count + "','" + price + "')";
+            MySqlCommand cmd = new MySqlCommand(command, con);
+            MySqlDataReader rdr = cmd.ExecuteReader();
+            con.Close();
+
+        }
+        public void AddProduct(Product product)
+        {
+            string name = product.Name;
+            int count = product.Count;
+            int price = product.Price;
+            int ml = product.Ml;
+            int capacity = product.Capacity;
+            try
+            {
+                con.Open();
+            }
+            catch { }
+            string command = "INSERT INTO products(name, quantity_item,capacity, price) VALUES('" + name + "','" + count + "','" + ml + "','" + capacity + "','" + price + "')";
+            MySqlCommand cmd = new MySqlCommand(command, con);
+            MySqlDataReader rdr = cmd.ExecuteReader();
+            con.Close();
+
+        }
+        public bool ProductExists(string name)
+        {
+            int l = 0;
+            try
+            {
+                con.Open();
+            }
+            catch { }
+            string command = "SELECT EXISTS(SELECT * FROM disposable_products where name like \"" + name + "\")";
+            MySqlCommand cmd = new MySqlCommand(command, con);
+            MySqlDataReader rdr = cmd.ExecuteReader();
+            while (rdr.Read())
+            {
+                l += rdr.GetInt32(0);
+            }
+            rdr.Close();
+            command = "SELECT EXISTS(Select *from products where name like \"" + name + "\")";
+            cmd = new MySqlCommand(command, con);
+            rdr = cmd.ExecuteReader();
+            while (rdr.Read())
+            {
+                l += rdr.GetInt32(0);
+            }
+
+            con.Close();
+            if (l != 0)
+                return true;
+            else
+                return false;
+        }
+        #endregion
     }
 }
